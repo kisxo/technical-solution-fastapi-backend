2bdd1e1c34fbf59f93b86e903de759cc35e5880c
uday <udaysubba2004@gmail.com>
2025-02-09 15:44:03 +0530

    add appwrite services

--- app/config/appwrite.py
+++ app/config/appwrite.py
@@ -1,5 +1,10 @@
-from appwrite.client import Client
 from app.config.config import settings
+from appwrite.client import Client
+from appwrite.services.account import Account
+from appwrite.services.users import Users
+from appwrite.services.databases import Databases
+from appwrite.services.storage import Storage
+
 
 client = Client()
 
@@ -9,3 +14,8 @@ client = Client()
   .set_key(settings.APPWRITE_API_KEY_SECRET) # Your secret API key
   .set_self_signed() # Use only on dev mode with a self-signed SSL cert
 )
+
+account = Account(client)
+users = Users(client)
+databases = Databases(client)
+storage = Storage(client)
