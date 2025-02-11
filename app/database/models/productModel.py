a527977dcdb5c81a14447c0fc193c9f1005969b6
uday <udaysubba2004@gmail.com>
2025-02-11 23:08:21 +0530

    add create and update product models

---
+++ app/database/models/productModel.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, BeforeValidator, Field
+from typing import Annotated, List, Optional
+
+PyObjectId = Annotated[str, BeforeValidator(str)]
+
+class ProductCreate(BaseModel):
+    name: str = Field(max_length=100)
+    price: float
+    buy_price: float
+    description: str = Field(max_length=500)
+    icon: str
+
+class ProductUpdate(BaseModel):
+    name: str | None = Field(max_length=100, default=None)
+    price: float | None = None
+    buy_price: float | None = None
+    description: str | None = Field(max_length=500, default=None)
+    icon: str | None = None
+    name: str | None = None
+    age: int | None = None
+    secret_name: str | None = None
