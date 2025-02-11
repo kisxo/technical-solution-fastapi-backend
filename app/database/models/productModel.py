d2506ebcf06e57f0dff04794c0654f53e83a5063
uday <udaysubba2004@gmail.com>
2025-02-12 01:07:43 +0530

    add file id and bucket id to product form

--- app/database/models/productModel.py
+++ app/database/models/productModel.py
@@ -7,15 +7,17 @@ class ProductCreate(BaseModel):
     name: str = Field(max_length=100)
     price: float
     buy_price: float
-    description: str = Field(max_length=500)
-    icon: str
+    description: str = Field(max_length=500, default=None)
+    file_id: str | None = None
+    bucket_id: str | None = None
 
 class ProductUpdate(BaseModel):
     name: str | None = Field(max_length=100, default=None)
     price: float | None = None
     buy_price: float | None = None
     description: str | None = Field(max_length=500, default=None)
-    icon: str | None = None
+    file_id: str | None = None
+    bucket_id: str | None = None
     name: str | None = None
     age: int | None = None
     secret_name: str | None = None
