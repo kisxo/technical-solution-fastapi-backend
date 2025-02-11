1074d4fbefd7a9e0050008d9d5d1bb8c345da934
uday <udaysubba2004@gmail.com>
2025-02-11 23:59:10 +0530

    add create product method

--- app/database/products.py
+++ app/database/products.py
@@ -1,5 +1,9 @@
+from fastapi import HTTPException
+from appwrite.exception import AppwriteException
+from appwrite.id import ID
 from app.config.appwrite import databases
 from app.database.config import technical_solution_db_id, products_collection_id
+from app.database.models import productModel
 
 async def list_products():
     result = databases.list_documents(
@@ -16,3 +20,16 @@ async def get_product(product_id: str):
         document_id= product_id,
     )
     return result
+
+async def create_product(product: productModel.ProductCreate):
+    try:
+        result = databases.create_document(
+            database_id = technical_solution_db_id,
+            collection_id = products_collection_id,
+            document_id = ID.unique(),
+            data = product.model_dump_json()
+        )
+        return result
+    except AppwriteException as e:
+        print(e)
+        raise HTTPException(status_code=404, detail=e.message)
