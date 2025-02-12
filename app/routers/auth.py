e0f938f73d5d01042ea2d541ed31c1b1188b6556
uday <udaysubba2004@gmail.com>
2025-02-12 14:53:25 +0530

    add set-cookie with session in response header

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,9 +1,17 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from app.accounts.auth import create_email_password_session
+from app.database.models import authModel
 
 router = APIRouter()
 
 @router.post("/login")
-async def login(email: str, password: str):
-    created_session = await create_email_password_session(email, password)
-    return {"status": "true", "message": "users route", 'data': created_session}
+async def login(
+    data: authModel.CreateEmailPasswordSession,
+    response: Response
+):
+
+    created_session = await create_email_password_session(data.email, data.password)
+
+    response.status_code = status.HTTP_200_OK
+    response.set_cookie("session", created_session["secret"])
+    return {"status": "true", "message": "Login Successful"}
