8df4fb3af7868dac62f404691f944e443ded1a6c
uday <udaysubba2004@gmail.com>
2025-02-13 01:24:26 +0530

    add admin auth check to file uploads

--- app/routers/files.py
+++ app/routers/files.py
@@ -1,11 +1,14 @@
-from fastapi import APIRouter, Response, status, File, UploadFile
+from fastapi import APIRouter, Response, status, File, UploadFile, Depends
 from app.storage import fileUtils
+from app.accounts.utils import get_admin_user
+from typing import Annotated
 
 router = APIRouter()
 
 @router.post("/")
 async def create_file(
     response: Response,
+    current_admin_user: Annotated[str, Depends(get_admin_user)],
     file: UploadFile = File(),
 ):
     
