4c229c4d38878fcdd6d4a55ec96a2207002a69f0
uday <udaysubba2004@gmail.com>
2025-02-13 22:11:43 +0530

    update return status code after account creation

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, status
 from app.database.models import authModel
 from app.accounts.users import create_email_account
 
@@ -6,7 +6,9 @@ router = APIRouter()
 
 @router.post("/")
 async def create_user(
-    user: authModel.CreateEmailPasswordAccountModel
+    user: authModel.CreateEmailPasswordAccountModel,
+    response: Response
 ):
     created_user = await create_email_account(user)
+    response.status_code = status.HTTP_201_CREATED
     return {"status": "true", "message": "users route", 'data': created_user}
