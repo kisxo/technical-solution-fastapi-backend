e6278e88e45cb8a49cbbb7752b8b6dd619d7bec8
uday <udaysubba2004@gmail.com>
2025-02-13 21:40:23 +0530

    add delete session route

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -19,6 +19,15 @@ async def generate_auth_token(
     response.set_cookie("session_token", created_session["secret"])
     return {"status": "true", "message": "Login Successful"}
 
+@router.delete("/token")
+async def delete_session(
+    response: Response
+):
+
+    response.status_code = status.HTTP_200_OK
+    response.delete_cookie("session_token")
+    return {"status": "true", "message": "Logout Successful"}
+
 @router.get("/user")
 async def check_auth_status_and_get_user(
     current_user: Annotated[str, Depends(get_current_user)]
